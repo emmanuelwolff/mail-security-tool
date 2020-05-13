faaabe224117497672c5b6ed0aaa63c65a19a4ca
emmanuel <emmanuel@hometalk.com>
2020-05-13 13:49:48 +0300

    use postgres

--- app/routes.php
+++ app/routes.php
@@ -14,17 +14,32 @@ return function (App $app) {
         return $response;
     });
 
-    //$app->get('/', function (Request $request, Response $response) {
-        //$htmlIndex = file_get_contents(__DIR__  . '/public/index.html');
-        //$response->getBody()->write($htmlIndex);
-        //return $response;
-    //});
+    $app->get('/', function (Request $request, Response $response) {
+        $htmlIndex = file_get_contents('index.html');
+        $response->getBody()->write($htmlIndex);
+        return $response;
+    });
 
     $app->group('/api', function (Group $group) {
         $group->put('/requests/{id}', function(Request $request, Response $response) {
             $response->getBody()->write('OK');
             return $response;
         });
-        $group->get('/requests', ListUsersAction::class);
+        $group->get('/requests', function(Request $request, Response $response){
+            $offset = $request->getQueryParams()['offset'] ?? 0;
+            $conn = pg_connect(getenv("DATABASE_URL"));
+            $result = pg_query($conn, "select * from mail_check_requests order by requested_at desc offset $offset limit 50"); 
+            $requests = pg_fetch_all($result);
+
+            $payload = [
+                'data' => [
+                    'requests' => $requests,
+                    'done' => (rand(1, 10) === 10) 
+                ]
+            ];
+            $json = json_encode($payload, JSON_PRETTY_PRINT);
+            $response->getBody()->write($json);
+            return $response->withHeader('Content-Type', 'application/json');
+        });
     });
 };
